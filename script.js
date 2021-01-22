6b96570634a1180aaaf15f3aa98fdeffe4e20524
Dmytro Borsuk <dmytro.borsuk03@gmail.com>
2021-01-22 21:37:24 +0200

    move script

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('Hello!');
+
+let button1 = document.querySelector('#button1');
+button1.addEventListener('click', () => { alert('Hello!') });
+button1.addEventListener('mouseover', () => { console.log('focus') });
+
+let button2 = document.querySelector('#button2');
+button2.addEventListener('click', () => { console.log('button 2 clicked!') } );
+button2.addEventListener('mousedown', () => { console.log('mousedown button2') } );
+button2.addEventListener('mouseup', () => { console.log('mouseup button2') } );
+
+let button3 = document.querySelector('#button3');
+button3.addEventListener('click', () => { 
+    console.log('button 3 clik');
+});
+
+ 
+
+
+
+
+
+
+
+
+
+
